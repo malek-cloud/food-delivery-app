96c3657044aea06f6d77ca33f1cea9f173aeab3f
malek-cloud <71387578+malek-cloud@users.noreply.github.com>
2020-10-14 23:54:01 +0100

    Update orderItem.dart

--- lib/widgets/orderItem.dart
+++ lib/widgets/orderItem.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as ord;
-import 'dart:math';
 
 class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
@@ -18,13 +17,17 @@ class _OrderItemState extends State<OrderItem> {
     return Card(
       margin: EdgeInsets.all(12),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ListTile(
             title: Text(
-              '\$${widget.order.amount.toStringAsFixed(2)}',
+              //'${widget.order.amount.toStringAsFixed(2)}€',
+              'L\'ordre du :',
+              style: TextStyle(color: Colors.black, fontSize: 19),
             ),
             subtitle: Text(
               DateFormat('dd/mm/yyyy  hh:mm').format(widget.order.dateTime),
+              style: TextStyle(color: Colors.teal, fontSize: 15),
             ),
             trailing: IconButton(
                 color: Colors.pink[900],
@@ -38,35 +41,50 @@ class _OrderItemState extends State<OrderItem> {
                 }),
           ),
           if (_expanded)
-            Container(
+            /* Container(
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 7),
-              height: min(widget.order.products.length * 20.0 + 20, 120),
-              child: ListView(
-                children: widget.order.products
+              //height: min(widget.order.products.length * 20.0 + 20, 120),
+              child:*/
+            /* widget.order.products
                     .map(
                       (prod) => Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            prod.title,
-                            style: TextStyle(
-                              fontFamily: 'KiteOne',
-                              fontSize: 17,
-                            ),
-                          ),
-                          Text('${prod.quantity}x \$${prod.price}',
-                              style: TextStyle(
-                                fontFamily: 'KiteOne',
-                                fontSize: 15,
-                              )),
-                        ],
-                      ),
-                    )
-                    .toList(),
+                        children:*/
+
+            /* Container(
+                  width: MediaQuery.of(context).size.width * 0.65,
+                  child:*/
+            Padding(
+              padding: const EdgeInsets.only(left: 18.0),
+              child: Text(
+                'Payement par : ${widget.order.paymentWay}',
+                softWrap: true,
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontFamily: 'KiteOne',
+                  fontSize: 17,
+                ),
               ),
-            )
+            ),
+          // ),
+          if (_expanded)
+            if (widget.order.adress != '')
+              Padding(
+                padding: const EdgeInsets.only(left: 18.0),
+                child: Text('à l\'adresse : ${widget.order.adress}',
+                    /*'${prod.quantity}x ${prod.price}€'*/
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontFamily: 'KiteOne',
+                      fontSize: 16,
+                    )),
+              ),
         ],
       ),
+      /*  ),
+                   .toList(),
+              ),
+            )*/
     );
   }
 }
