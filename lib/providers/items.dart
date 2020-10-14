5d4ab1fc0e700193732ffd865e99805b1bae50e8
malek-cloud <71387578+malek-cloud@users.noreply.github.com>
2020-10-14 23:43:58 +0100

    Update items.dart

--- lib/providers/items.dart
+++ lib/providers/items.dart
@@ -1,54 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'item_structure.dart';
+import 'package:http/http.dart' as http;
 
 class Items with ChangeNotifier {
-  List<Item> _itemsList = [
-    Item(
-      id: 'p1',
-      title: 'Salad',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLmU5xXKu9mtLzD-mN6h9IoSDJ_PFH4QWKRw&usqp=CAU',
-      category: 'POKE & BOWL',
-    ),
-    Item(
-      id: 'p2',
-      title: 'Hamberger',
-      description: 'A nice pair of trousers.',
-      price: 4.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS24wvA9ozipJc5-IStQrqZIo_a3urpEZGIGA&usqp=CAU',
-      category: 'WOK & AUTRES PLATS',
-    ),
-    Item(
-      id: 'p3',
-      title: 'makrouna',
-      description: 'A nice pair of trousers.',
-      price: 9.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNbG0DNfCqEl0RwzNQb11QUG4Edsna7ESCsg&usqp=CAU',
-      category: 'SUSHIS',
-    ),
-    Item(
-      id: 'p4',
-      title: 'Pizza',
-      description: 'A nice pair of trousers.',
-      price: 6.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSZWdDdAtDKxPpJhAfzRP6DjA-PCRmZHn38ag&usqp=CAU',
-      category: 'DESSERTS',
-    ),
-    Item(
-      id: 'p5',
-      title: 'chakchouka',
-      description: 'A nice pair of trousers.',
-      price: 5.99,
-      imageUrl:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRpka_F-Iz5svr0GM_1rhKi9ja2x3zREuLK-g&usqp=CAU',
-      category: 'SUSHIS',
-    ),
-  ];
+  List<Item> _itemsList = [];
+  Future<void> getData() async {
+    const url = 'https://apisite.klikx.fr/public/v1/products/list';
+
+    final jsonData = await http.get(url);
+    final jsonResponse = json.decode(jsonData.body) as List;
+    final List<Item> loadedProduct = [];
+    for (var i = 0; i < jsonResponse.length; i++) {
+      loadedProduct.add(Item(
+          id: jsonResponse[i]['_id'],
+          title: jsonResponse[i]['name'],
+          description: jsonResponse[i]['description'],
+          price: jsonResponse[i]['price'].toDouble(),
+          imageUrl: jsonResponse[i]['picture'],
+          category: jsonResponse[i]['category']['name'],
+          options: jsonResponse[i]['options'],
+          supplements: jsonResponse[i]['supplements']));
+    }
+
+    _itemsList = loadedProduct;
+    //print(jsonResponse);
+    notifyListeners();
+  }
+
   List<Item> get items {
     return [..._itemsList];
   }
